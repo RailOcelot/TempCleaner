95c6469dc6dcbd17781d5a40a2c085cbe70fe65a
RailOcelot <RailOcelot@gmail.com>
2018-03-26 07:38:01 -0500

    Update tempclean.bat

--- tempclean.bat
+++ tempclean.bat
@@ -1,30 +1,45 @@
+:: JOB: Clear Windows Temp Data
 rundll32.exe inetcpl.cpl,ClearMyTracksByProcess 4351
-del /F /S /Q "%USERPROFILE%\*.blf" 2>NUL
-del /F /S /Q "%USERPROFILE%\*.regtrans-ms" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\LocalLow\Sun\Java\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Cache\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\JumpListIconsOld\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\JumpListIcons\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Local Storage\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Media Cache\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Internet Explorer\Recovery\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Terminal Server Client\Cache\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Caches\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Explorer\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\History\low\*" /AH 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\INetCache\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\WER\ReportArchive\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\WER\ReportQueue\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Microsoft\Windows\WebCache\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Local\Temp\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Roaming\Adobe\Flash Player\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Roaming\Macromedia\Flash Player\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Recent\*" 2>NUL
-del /F /S /Q "%USERPROFILE%\Recent\*" 2>NUL
-del /F /Q "%USERPROFILE%\Documents\*.tmp" 2>NUL
-del /F /Q "%USERPROFILE%\Downloads\*.tmp" 2>NUL
-del /F /Q "%USERPROFILE%\Pictures\*.tmp" 2>NUL
+for /D %%x in ("%SystemDrive%\Users\*") do ( 
+		del /F /S /Q "%%x\*.blf" 2>NUL
+		del /F /S /Q "%%x\*.regtrans-ms" 2>NUL
+		del /F /S /Q "%%x\AppData\LocalLow\Sun\Java\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Google\Chrome\User Data\Default\Cache\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Google\Chrome\User Data\Default\JumpListIconsOld\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Google\Chrome\User Data\Default\JumpListIcons\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Google\Chrome\User Data\Default\Local Storage\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Google\Chrome\User Data\Default\Media Cache\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Internet Explorer\Recovery\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Terminal Server Client\Cache\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\Caches\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\Explorer\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\History\low\*" /AH 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\INetCache\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\WER\ReportArchive\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\WER\ReportQueue\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Microsoft\Windows\WebCache\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Local\Temp\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Roaming\Adobe\Flash Player\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Roaming\Macromedia\Flash Player\*" 2>NUL
+		del /F /S /Q "%%x\AppData\Roaming\Microsoft\Windows\Recent\*" 2>NUL
+		del /F /S /Q "%%x\Recent\*" 2>NUL
+		del /F /Q "%%x\Documents\*.tmp" 2>NUL
+	)
+:: JOB: Root drive garbage (usually C drive)
+rmdir /S /Q %SystemDrive%\Temp 2>NUL
+for %%i in (bat,txt,log,jpg,jpeg,tmp,bak,backup,exe) do (
+	del /F /Q "%SystemDrive%\*.%%i" 2>NUL
+)
+
+:: JOB: Remove files left over from installing Nvidia/ATI/AMD/Dell/Intel/HP drivers
+for %%i in (NVIDIA,ATI,AMD,Dell,Intel,HP) do (
+	rmdir /S /Q "%SystemDrive%\%%i" 2>NUL
+)
+:: JOB: Clear additional unneeded files from NVIDIA driver installs
+if exist "%ProgramFiles%\Nvidia Corporation\Installer2" rmdir /s /q "%ProgramFiles%\Nvidia Corporation\Installer2"
+if exist "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService" del /f /q "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService\*.exe"
+:: JOB: System temp files
 del /F /S /Q "%WINDIR%\TEMP\*" 2>NUL
 :: JOB: Remove the Office installation cache. Usually around ~1.5 GB
 if exist %SystemDrive%\MSOCache rmdir /S /Q %SystemDrive%\MSOCache
@@ -46,3 +61,18 @@ if exist "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportQueue" rmdir /s /q "%ALL
 
 :: JOB: Clear Windows Search Temp Data
 if exist "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp"
+
+:: JOB: Clear Windows Defender Scan Results
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick"
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource"
+:: JOB: Windows update logs & built-in backgrounds (space waste)
+del /F /Q %WINDIR%\*.log 2>NUL
+del /F /Q %WINDIR%\*.txt 2>NUL
+del /F /Q %WINDIR%\*.bmp 2>NUL
+del /F /Q %WINDIR%\*.tmp 2>NUL
+del /F /Q %WINDIR%\Web\Wallpaper\*.* 2>NUL
+rmdir /S /Q %WINDIR%\Web\Wallpaper\Dell 2>NUL
+
+:: JOB: Windows CBS logs
+::      these only exist on Vista and up, so we look for "Microsoft", and assuming we don't find it, clear out the folder
+del /F /Q %WINDIR%\Logs\CBS\* 2>NUL
